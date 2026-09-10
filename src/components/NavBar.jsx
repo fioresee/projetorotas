80b645ee1ccfea82b2ec044d9aca013e172dc025
Arthur Fiorese <arthur.herrada@gmail.com>
2026-09-10 09:51:07 -0300

    style: Estilização da NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,30 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import {Home, Info, Phone, Wrench} from 'lucide-react'
+
 
 const NavBar = () => {
   return (
-    <>
-      <Link to='/'>Home</Link>
-      <Link to='/sobre'>Sobre</Link>
-      <Link to='/produto'>Produto</Link>
-      <Link to='/contato'>Contato</Link>
-    </>
+    <nav className=" bg-blue-500 shadow-md">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <span className="text-xl font-bold text-white">Projeto</span>
+          </div>
+        </div>
+
+        <div className=" flex space-x-4 items-center">
+          <Link to="/" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Home/>Home</Link>
+          <Link to="/sobre" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Info/>Sobre</Link>
+          <Link to="/produto" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Wrench />Produto</Link>
+          <Link to="/contato" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Phone />Contato</Link>
+
+        </div>
+
+
+
+      </div>
+
+    </nav>
   )
 }
 
