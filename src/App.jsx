4dd754f12fdadb375a69c9d09c7d3e1c01323915
Arthur Fiorese <arthur.herrada@gmail.com>
2026-09-10 09:33:04 -0300

    feat: Criação de rotas do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Produto from './pages/Produto'
+import Contato from './pages/Contato'
+
+
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-50 text-gray-900">
+        <NavBar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sobre" element={<Sobre/>}/>
+            <Route path="/produto" element={<Produto/>}/>
+            <Route path="/contato" element={<Contato/>}/>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom'
+
 const NavBar = () => {
   return (
     <>
-      
+      <Link to='/'>Home</Link>
+      <Link to='/sobre'>Sobre</Link>
+      <Link to='/produto'>Produto</Link>
+      <Link to='/contato'>Contato</Link>
     </>
   )
 }
